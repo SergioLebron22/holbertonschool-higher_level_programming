d4fd6c0e8bb9cc89c40e0f903317a8787f1158bd
SergioLebron22 <lebronsergio22@gmail.com>
2024-05-10 09:38:37 -0400

    fixed args program

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,7 +2,11 @@
 import sys
 
 def main():
-    print("{} arguments".format(len(sys.argv) - 1))
+    
+    if len(sys.argv) == 2:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(len(sys.argv) - 1))
 
     for i in range(1, len(sys.argv)):
         print("{}: {}".format(i, sys.argv[i]))
