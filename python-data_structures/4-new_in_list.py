fdff9b469793c0724cfda5f8a78d96e96fc41ee8
SergioLebron22 <lebronsergio22@gmail.com>
2024-05-13 09:57:20 -0400

    fixed new in list function

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
 def new_in_list(my_list, idx, element):
-    new_list = my_list
+    new_list = my_list.copy()
 
     if idx < 0:
         return new_list
-    elif idx > len(my_list) - 1:
+    elif idx > len(new_list) - 1:
         return new_list
     
     for i in range(len(new_list)):
