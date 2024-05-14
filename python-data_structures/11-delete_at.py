a5cf707117e51b22cd1cf8a967f0baf43f4df768
SergioLebron1 <sergio.lebron1@upr.edu>
2024-05-13 22:19:55 -0400

    fix: delete at function

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 
 def delete_at(my_list=[], idx=0):
-    if len(my_list) < 0:
-        return my_list
-    elif idx > len(my_list) - 1:
+    if idx < 0 or idx >= len(my_list):
         return my_list
 
     new_list = my_list
-
-    del (new_list[idx])
+    del new_list[idx]
 
     return new_list
