ef9318258c59339e51cfdf89acd84a1981275f32
SergioLebron22 <lebronsergio22@gmail.com>
2024-05-14 14:46:50 -0400

    fix: pycodestyle

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,7 @@
 def roman_to_int(roman_string):
     if roman_string == None:
         return 0
-    
+
     result = 0
     roman_num = {
         "I": 1,
@@ -14,9 +14,9 @@ def roman_to_int(roman_string):
         "D": 500,
         "M": 1000
     }
-    
+
     for i in range(len(roman_string)):
-        
+
         current = roman_num[roman_string[i]]
         prev = roman_num[roman_string[i - 1]]
 
