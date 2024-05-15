2b64c691bf6eb72d57a320e25592db5522e70692
SergioLebron22 <lebronsergio22@gmail.com>
2024-05-15 14:56:49 -0400

    fix: pycodestyle

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -2,7 +2,7 @@
 
 def list_division(my_list_1, my_list_2, list_length):
     result = []
-    try:   
+    try:
         for i in range(list_length):
             try:
                 result.append(my_list_1[i] / my_list_2[i])
