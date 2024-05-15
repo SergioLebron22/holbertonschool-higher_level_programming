3a6b2975601bc0044de1e56129a5a459ba0e7617
SergioLebron22 <lebronsergio22@gmail.com>
2024-05-15 12:01:06 -0400

    Added safe print list integer function

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    counter = 0
+    try:
+        for num in range(x):
+            try:
+                print("{:d}".format(my_list[num]), end='')
+                counter += 1
+            except (ValueError, TypeError):
+                continue
+        print()
+        return counter
+    except (ValueError, TypeError):
+        print()
+        
+        
