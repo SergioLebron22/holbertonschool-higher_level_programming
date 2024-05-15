2ebcd23d01e8b1b49fb30de6332ff8e6a916c460
SergioLebron22 <lebronsergio22@gmail.com>
2024-05-15 12:04:01 -0400

    fix: redundant code in safe print list int function

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -2,16 +2,12 @@
 
 def safe_print_list_integers(my_list=[], x=0):
     counter = 0
-    try:
-        for num in range(x):
-            try:
-                print("{:d}".format(my_list[num]), end='')
-                counter += 1
-            except (ValueError, TypeError):
-                continue
-        print()
-        return counter
-    except (ValueError, TypeError):
-        print()
-        
-        
+
+    for num in range(x):
+        try:
+            print("{:d}".format(my_list[num]), end='')
+            counter += 1
+        except (ValueError, TypeError):
+            continue
+    print()
+    return counter
