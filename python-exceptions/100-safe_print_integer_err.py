d20c49d19d005230f69b6cb3613c4250a2cfc5be
SergioLebron22 <lebronsergio22@gmail.com>
2024-05-15 16:25:29 -0400

    fixing 100

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -5,7 +5,7 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except (ValueError, TypeError):
+    except (TypeError, ValueError):
         sys.stderr.write("Exception: Unknown format code 'd'\
  for object of type 'str'\n")
         return False
