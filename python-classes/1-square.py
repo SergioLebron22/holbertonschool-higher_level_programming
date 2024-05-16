41652833dacf67e1b5bfd867d06bb347b8082624
SergioLebron22 <lebronsergio22@gmail.com>
2024-05-16 12:33:21 -0400

    Added constructor to square class

---
+++ python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+this file contains the Square class
+"""
+
+
+class Square:
+    """
+    A class representing a square.
+
+    Attributes:
+        None
+
+    Methods:
+        None
+    """
+    def __init__(self, size):
+        self.__size = size
