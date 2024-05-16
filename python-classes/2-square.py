1aa397e74a5c6b65f2ba684a890ec45d0b396624
SergioLebron22 <lebronsergio22@gmail.com>
2024-05-16 14:45:31 -0400

    Added validation in class init

---
+++ python-classes/2-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+this file contains the Square class
+"""
+
+
+class Square:
+    """
+    A class representing a square.
+
+    Attributes:
+        __size: size of the square
+
+    Methods:
+        None
+    """
+    def __init__(self, size=0):
+
+        if not isinstance(size, int):
+             raise TypeError("size must be an integer")
+        if size < 0:
+               raise ValueError("size must be >= 0")
+        self.__size = size
