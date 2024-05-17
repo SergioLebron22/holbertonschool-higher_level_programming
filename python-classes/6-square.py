9ae93905ad95daeacd1ffde59d1c1d4e92b96bea
SergioLebron1 <sergio.lebron1@upr.edu>
2024-05-16 23:57:49 -0400

    adding position task attempt to repo

---
+++ python-classes/6-square.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python3
+"""
+this file contains the Square class
+"""
+
+
+class Square:
+    """
+    Represents a square.
+
+    Attributes:
+        __size (int): The size of the square.
+
+    Methods:
+        __init__(self, size=0): Initializes a Square object.
+        area(self): Calculates the area of the square.
+    """
+
+    def __init__(self, size=0, position=(0, 0)):
+        """
+        Initializes a Square object.
+
+        Args:
+            size (int): The size of the square. Defaults to 0.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+        self.__position = position
+
+    def area(self):
+        """
+        Calculates the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        if not isinstance(self.__size, int):
+            raise TypeError
+        if self.__size < 0:
+            raise ValueError
+
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if self.__size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#", end="")
+            print()
+
+    # @property
+    # def position(self):
+    #     return self.__position
+
+    # @position.setter
+    # def position(self, value):
+    #     if not isinstance(value, tuple):
+    #         for i in value:
+    #             if i < 0:
+    #                 raise TypeError("position must be a tuple of 2 positive integers")
+    #     self.__position = value
+
