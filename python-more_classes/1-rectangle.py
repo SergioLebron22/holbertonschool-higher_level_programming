beae66eb6c058483db0b3a03f95e04cc131eda7f
SergioLebron22 <lebronsergio22@gmail.com>
2024-05-21 09:27:22 -0400

    fix: __init__ function parameter verification

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -18,6 +18,14 @@ class Rectangle:
             height (int): The height of the rectangle.
             width (int): The width of the rectangle.
         """
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
         self.__height = height
         self.__width = width
 
