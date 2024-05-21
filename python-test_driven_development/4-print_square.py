5f3050946419fb26df89daad6a14c7e8f2dc9349
SergioLebron22 <lebronsergio22@gmail.com>
2024-05-20 23:18:03 -0400

    fix: documentation and code style

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,13 +1,31 @@
 #!/usr/bin/python3
+"""
+this file contains print_square function
+"""
+
 
 def print_square(size):
+    """
+    Prints a square made of '#' characters with the given size.
+
+    Args:
+        size (int): The size of the square.
+
+    Raises:
+        TypeError: If size is not an integer.
+        ValueError: If size is less than 0.
+
+    Returns:
+        None
+    """
+
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
     if isinstance(size, float) and size < 0:
         raise TypeError("size must be an integer")
-    
+
     for i in range(size):
         for j in range(size):
             print("#", end="")
