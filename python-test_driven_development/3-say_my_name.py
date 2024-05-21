952d1d6f440243c2b9a363aa34b015366feaa4cc
SergioLebron22 <lebronsergio22@gmail.com>
2024-05-20 22:58:49 -0400

    fix: pycodestyle

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -19,9 +19,9 @@ def say_my_name(first_name, last_name=""):
     Returns:
         None
     """
-    if not isinstance(first_name, str): 
+    if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-    
+
     print(f"My name is {first_name} {last_name}")
