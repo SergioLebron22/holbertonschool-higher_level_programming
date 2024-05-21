94b70c2a7695b5daf1ceb1ec0b7e7899bc1698ab
SergioLebron22 <lebronsergio22@gmail.com>
2024-05-20 21:36:51 -0400

    feat: added matrix divided function and doctest txt file

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -10,17 +10,17 @@ def matrix_divided(matrix, div):
     Divide all elements of a matrix by a given divisor.
 
     Args:
-        matrix (list): A matrix represented as a list 
+        matrix (list): A matrix represented as a list
         of lists containing integers or floats.
         div (int or float): The divisor.
 
     Raises:
         ZeroDivisionError: If the divisor is zero.
-        TypeError: If the divisor is not a number 
+        TypeError: If the divisor is not a number
         or if the matrix is not a valid matrix.
 
     Returns:
-        list: A new matrix with the elements divided by 
+        list: A new matrix with the elements divided by
         the divisor, rounded to 2 decimal places.
     """
 
@@ -33,7 +33,7 @@ def matrix_divided(matrix, div):
     for row in matrix:
         if not isinstance(matrix, list) or not isinstance(row, list):
             raise TypeError("matrix must be a matrix\
-                        (list of lists) of integers/floats")
+ (list of lists) of integers/floats")
 
         row_len = len(matrix[0])
         if len(row) != row_len:
@@ -41,7 +41,7 @@ def matrix_divided(matrix, div):
         for num in row:
             if not isinstance(num, (int, float)):
                 raise TypeError("matrix must be a matrix\
-                        (list of lists) of integers/floats")   
+ (list of lists) of integers/floats")
             new_matrix.append(round(num / div, 2))
 
     return new_matrix
