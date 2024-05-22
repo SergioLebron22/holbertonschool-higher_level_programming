e0c69349b079ca205925ed7d838a9f8136e41397
SergioLebron22 <lebronsergio22@gmail.com>
2024-05-22 16:12:09 -0400

    fix: integr validator to raise type error if bool is passed as value

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -13,6 +13,8 @@ class BaseGeometry:
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
+        if isinstance(value, bool):
+            raise TypeError(f"{name} must be an integer")
         if not isinstance(value, int):
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
