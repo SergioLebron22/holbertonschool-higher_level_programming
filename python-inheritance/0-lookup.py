4cc3a75e9055c942122d867db508274f75d77e04
SergioLebron22 <lebronsergio22@gmail.com>
2024-05-22 10:18:07 -0400

    fix: pycodestyle

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -7,12 +7,14 @@ this file contains the function lookup
 
 def lookup(obj):
     """
-    Returns a list of all attributes and methods of the given object.
+    Returns a list of all attributes
+    and methods of the given object.
 
     Args:
         obj: The object to inspect.
 
     Returns:
-        A list of strings representing the attributes and methods of the object.
+        A list of strings representing the
+        attributes and methods of the object.
     """
     return dir(obj)
