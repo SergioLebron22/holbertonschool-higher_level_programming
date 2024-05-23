674b9d157242496fb5cb78da25c726230162c550
SergioLebron22 <lebronsergio22@gmail.com>
2024-05-22 22:48:57 -0400

    fix: pycodestyle

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -10,8 +10,10 @@ class BaseGeometry:
     This is the base class for geometry objects.
 
     Methods:
-    - area(): Raises an exception indicating that the method is not implemented.
-    - integer_validator(name, value): Validates if the given value is an integer and greater than 0.
+    - area(): Raises an exception indicating 
+    that the method is not implemented.
+    - integer_validator(name, value): Validates 
+    if the given value is an integer and greater than 0.
     """
 
     def area(self):
