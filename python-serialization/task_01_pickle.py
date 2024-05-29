2c0fb0340df8dc4d8de4d481aa59725b47b1dbb6
SergioLebron22 <lebronsergio22@gmail.com>
2024-05-29 11:01:14 -0400

    Added CustomObject class

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+
+import pickle
+
+
+class CustomObject:
+
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        print(f"Name: {self.name}\nAge: {self.age}\nIs Student: {self.is_student}")
+
+    def serialize(self, filename):
+        pickle.dump(self, open(filename, "wb"))
+
+    @classmethod
+    def deserialize(cls, filename):
+        cls = pickle.load(open(filename, "rb"))
+        return cls
