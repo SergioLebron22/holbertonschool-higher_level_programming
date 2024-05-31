e641385b5cd0e4f66e4869296f322f9c2c48623e
SergioLebron22 <lebronsergio22@gmail.com>
2024-05-31 15:45:13 -0400

    refactor HTTP server response content type to use text/plain

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-
 import http.server
 import json
 from http.server import HTTPServer, BaseHTTPRequestHandler
@@ -10,7 +9,7 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path == '/':
             self.send_response(200)
-            self.send_header("Content-type", "text/html")
+            self.send_header("Content-type", "text/plain")
             self.end_headers()
             self.wfile.write(b"Hello, this is a simple API!")
         elif self.path == '/data':
@@ -27,7 +26,7 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header("Content-type", "application/json")
             self.end_headers()
-            self.wfile.write(b"OK")
+            self.wfile.write(b'OK')
         elif self.path == '/info':
             self.send_response(200)
             self.send_header("Content-type", "application/json")
@@ -43,13 +42,12 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             self.end_headers
             self.wfile.write("404 Not Found")
 
-def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler):
-    server_address = ('', 8000)
+def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler, port=8000):
+    server_address = ('', port)
     httpd = server_class(server_address, handler_class)
+    print(f'Starting httpd server on port {port}...')
     httpd.serve_forever()
 
 if __name__ == "__main__":
     run()
-
-
             
