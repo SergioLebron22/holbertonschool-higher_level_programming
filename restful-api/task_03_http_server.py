b2628782d12f78a5febbd122e4866f36ae1c4cd3
SergioLebron22 <lebronsergio22@gmail.com>
2024-05-31 15:57:12 -0400

    refactor HTTP server response content type to use text/plain

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -40,12 +40,11 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             self.send_response(404)
             self.send_header("Content-type", "application/json")
             self.end_headers
-            self.wfile.write("404 Not Found")
+            self.wfile.write(b"404 Not Found")
 
-def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler, port=8000):
-    server_address = ('', port)
+def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler):
+    server_address = ('', 8000)
     httpd = server_class(server_address, handler_class)
-    print(f'Starting httpd server on port {port}...')
     httpd.serve_forever()
 
 if __name__ == "__main__":
