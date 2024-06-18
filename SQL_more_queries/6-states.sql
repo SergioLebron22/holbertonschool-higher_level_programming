caea17726118e6309303f372193bfeb4becc9156
SergioLebron22 <lebronsergio22@gmail.com>
2024-06-18 13:24:05 -0400

    Refactor SQL script to create table for states

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,6 @@
+-- creates table states in database hbtn_0d_usa
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+    id INT NOT NULL UNIQUE AUTO_INCREMENT,
+    name VARCHAR(256)
+)
