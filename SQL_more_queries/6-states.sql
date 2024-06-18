cb1185289b4ec32cd4495e6d8ca94c560c01e754
SergioLebron22 <lebronsergio22@gmail.com>
2024-06-18 13:46:52 -0400

    Refactor SQL script to update states table with primary key constraint on id column

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- creates table states in database hbtn_0d_usa
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_2;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
     id INT UNIQUE AUTO_INCREMENT NOT NULL,
     name VARCHAR(256) NOT NULL,
