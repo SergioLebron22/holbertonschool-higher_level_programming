8cc05275b182fdcadce525861a8f909e40e1c0d7
SergioLebron22 <lebronsergio22@gmail.com>
2024-06-19 11:18:28 -0400

    chore: Connect to MySQL database and retrieve states' id and name

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
 
+"""
+This script connects to a MySQL database and
+retrieves the id and name of all states
+from the 'states' table.
+"""
+
+
 import MySQLdb
 import sys
+
 
 db = MySQLdb.connect(host='localhost', user=sys.argv[1], password='password', db=sys.argv[3])
 cur = db.cursor()
