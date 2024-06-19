2222189fb0534d45a66c2960098bd1c558875e70
SergioLebron22 <lebronsergio22@gmail.com>
2024-06-19 11:59:46 -0400

    chore: Refactor Python script to select and print all states from the database

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -10,11 +10,16 @@ from the 'states' table.
 import MySQLdb
 import sys
 
+if __name__ == "__main__":
+    db = MySQLdb.connect(host='localhost', user=sys.argv[1], password='password', db=sys.argv[3])
+    cur = db.cursor()
 
-db = MySQLdb.connect(host='localhost', user=sys.argv[1], password='password', db=sys.argv[3])
-cur = db.cursor()
+    cur.execute("""SELECT id, name FROM states ORDER BY states.id ASC""")
+    rows = cur.fetchall()
+    for row in rows:
+        print(f"{row}")
+    
+    cur.close()
+    db.close()
 
-cur.execute("""SELECT id, name FROM states ORDER BY states.id ASC""")
-rows = cur.fetchall()
-for row in rows:
-    print(f"{row}")
+    
