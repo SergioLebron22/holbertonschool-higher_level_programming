18a848b15447547eb82f5d10c73410b0328c071c
SergioLebron22 <lebronsergio22@gmail.com>
2024-06-19 13:12:31 -0400

    chore: Update MySQL connection parameters in Python script

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -14,8 +14,8 @@ import sys
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost', port=3306,
-                        user=sys.argv[1],
-                        password='password', db=sys.argv[3])
+                         user=sys.argv[1],
+                         password=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
 
     cur.execute("""SELECT id, name FROM states ORDER BY states.id ASC""")
