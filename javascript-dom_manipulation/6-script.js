e5add5bef40b93642e20b8f987ac08baf52eb5c9
SergioLebron22 <lebronsergio22@gmail.com>
2024-07-28 16:51:13 -0400

    feat: Fetch and display character name from Star Wars API on page load

---
+++ javascript-dom_manipulation/6-script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const url = 'https://swapi-api.hbtn.io/api/people/5/?format=json';
+
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        const name = data.name;
+        document.getElementById('character').textContent = name;
+    })
+    .catch(error => {
+        console.error('Error fetching data:', error);
+    });
+});
